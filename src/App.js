37a99798f3ac9401c4cad449a39e98d4bac12c7c
Josiah <Josiahsingh1996@gmail.com>
2019-06-12 13:33:53 +0300

    updated css

--- src/App.js
+++ src/App.js
@@ -1,43 +1,54 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch, Link} from 'react-router-dom';
-import './App.css';
-import Customers from './Components/Customers';
-import Trainings from './Components/Trainings';
-import Calendar from './Components/Calendar'
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import "./App.css";
+import Customers from "./Components/Customers";
+import Trainings from "./Components/Trainings";
+import Calendar from "./Components/CalendarView";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <header className="App-header">
-      <h1>Gym Database</h1>
-      </header>
-      <BrowserRouter>
-      <div>
-      <ul class="nav justify-content-center">
-       <li class="nav-item">
-       <Link className="nav-link" to="/">Frontpage</Link>{' '}
-      </li>
-      <li class="nav-item">
-      <Link  className="nav-link" to="/customers">Customers</Link>{' '}
-      </li>
-      <li class="nav-item">
-      <Link className="nav-link" to="/trainings">Trainings</Link>{' '}
-     </li>
-     <li class="nav-item">
-     <Link className="nav-link" to="/calendar">Calendar</Link>{' '}
-     </li>
-    </ul>
-        <Switch>
-          <Route exact path = "/" render={() => <h3>Welcome</h3>} />
-          <Route path = "/customers" component={Customers} />
-          <Route path = "/trainings" component={Trainings} />
-          <Route path = "/calendar" component={Calendar} />
-        </Switch>
-        </div>
-      </BrowserRouter>
+        <header className="App-header">
+          <h1>Gym Database</h1>
+        </header>
+        <BrowserRouter>
+          <div className="nav_main">
+            <ul className="nav justify-content-center">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Frontpage
+                </Link>{" "}
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/customers">
+                  Customers
+                </Link>{" "}
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/trainings">
+                  Trainings
+                </Link>{" "}
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/calendar">
+                  Calendar
+                </Link>{" "}
+              </li>
+            </ul>
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={() => <h3>Welcome! We haven't seen you in a while.</h3>}
+              />
+              <Route path="/customers" component={Customers} />
+              <Route path="/trainings" component={Trainings} />
+              <Route path="/calendar" component={Calendar} />
+            </Switch>
+          </div>
+        </BrowserRouter>
       </div>
-  
     );
   }
 }
--- src/Components/Customers.js
+++ src/Components/Customers.js
@@ -1,190 +1,204 @@
-import React, { Component } from 'react';
-import ReactTable from 'react-table';
+import React, { Component } from "react";
+import ReactTable from "react-table";
 import "react-table/react-table.css";
-import { ToastContainer, toast } from 'react-toastify';
-import { confirmAlert } from 'react-confirm-alert';
-import 'react-confirm-alert/src/react-confirm-alert.css'
-import SkyLight from 'react-skylight';
-import AddCustomers from './AddCustomers';
-import EditCustomers from './EditCustomers';
-import Trainings from './Trainings';
-import AddTrainings from './AddTrainings'
-
-
+import { ToastContainer, toast } from "react-toastify";
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
+import SkyLight from "react-skylight";
+import AddCustomers from "./AddCustomers";
+import EditCustomers from "./EditCustomers";
+import Trainings from "./Trainings";
+import AddTrainings from "./AddTrainings";
 
 class Customers extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            customers: [],
+  constructor(props) {
+    super(props);
+    this.state = {
+      customers: []
+    };
+  }
+
+  componentDidMount() {
+    this.showCustomers();
+  }
+
+  // LOADS CUSTOMERS
+  showCustomers = () => {
+    fetch("https://customerrest.herokuapp.com/api/customers")
+      .then(response => response.json())
+      .then(responseData => {
+        // console.log(responseData.content)
+        this.setState({ customers: responseData.content });
+      });
+  };
+  // ADDS CUSTOMERS
+  addCustomers = newCustomer => {
+    fetch("https://customerrest.herokuapp.com/api/customers", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newCustomer)
+    })
+      .then(response => this.showCustomers())
+      .catch(err => console.log(err));
+
+    toast.success("Added!", {
+      position: toast.POSITION.TOP_CENTER
+    });
+  };
+
+  // DELETES CUSTOMER
+  deleteCustomer = value => {
+    confirmAlert({
+      message: "Do you want to proceed?",
+      buttons: [
+        {
+          label: "Yes",
+          onClick: () => {
+            fetch(value, { method: "DELETE" })
+              .then(response => this.showCustomers())
+              .catch(err => console.error(err));
+
+            toast.success("Deleted!", {
+              position: toast.POSITION.TOP_CENTER
+            });
+          }
+        },
+        {
+          label: "No"
         }
-        
-    }
+      ]
+    });
+  };
 
-    componentDidMount() {
-        this.showCustomers();
-    }
+  // UPDATES CUSTOMER
+  updateCustomer = (link, editcustomer) => {
+    fetch(link, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(editcustomer)
+    })
+      .then(response => this.showCustomers())
+      .catch(err => console.error(err));
 
+    toast.success("Updated", {
+      position: toast.POSITION.TOP_CENTER
+    });
+  };
 
-    // LOADS CUSTOMERS
-    showCustomers = () => {
-        fetch('https://customerrest.herokuapp.com/api/customers')
-        .then(response => response.json())
-        .then(responseData => {
-            // console.log(responseData.content)
-            this.setState({customers: responseData.content})
-        })
-    }
-    // ADDS CUSTOMERS 
-    addCustomers = (newCustomer) => {
-        fetch('https://customerrest.herokuapp.com/api/customers', {
-            method : 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(newCustomer)
-        })
-        .then(response => this.showCustomers())
-        .catch(err => console.log(err))
+  // ADDS TRAINING
+  addTrainings = newTraining => {
+    fetch("https://customerrest.herokuapp.com/api/trainings", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newTraining)
+    });
+    toast.success("Training Added!", {
+      position: toast.POSITION.TOP_CENTER
+    });
+  };
 
-        toast.success("Added!", {
-            position: toast.POSITION.TOP_CENTER
-        });
-    }
-
-    // DELETES CUSTOMER
-    deleteCustomer = (value) => {
-        confirmAlert({
-            message: 'Do you want to proceed?',
-            buttons: [
+  render() {
+    return (
+      <div>
+        <div className="addbtn_Customer">
+          <AddCustomers addCustomers={this.addCustomers} />
+        </div>
+        <ReactTable
+          data={this.state.customers}
+          columns={[
+            {
+              columns: [
                 {
-                    label: 'Yes',
-                    onClick: () => {
-                        fetch(value, {method: 'DELETE'})
-                        .then(response => this.showCustomers())
-                        .catch(err => console.error(err))
-
-                        toast.success("Deleted!", {
-                            position: toast.POSITION.TOP_CENTER
-                        });
-                    }
+                  Header: "FirstName",
+                  accessor: "firstname"
                 },
                 {
-                    label: 'No',
+                  Header: "LastName",
+                  accessor: "lastname"
+                },
+                {
+                  Header: "StreetAddress",
+                  accessor: "streetaddress"
+                },
+                {
+                  Header: "PostCode",
+                  accessor: "postcode"
+                },
+                {
+                  Header: "City",
+                  accessor: "city"
+                },
+                {
+                  Header: "Email",
+                  accessor: "email"
+                },
+                {
+                  Header: "Phone",
+                  accessor: "phone"
+                },
+                {
+                  id: "addTraining",
+                  sortable: false,
+                  filterable: false,
+                  width: 170,
+                  accessor: "links.0.href",
+                  Cell: ({ value }) => (
+                    <AddTrainings
+                      addTrainings={this.addTrainings}
+                      link={value}
+                    />
+                  )
+                },
+                {
+                  id: "button",
+                  sortable: false,
+                  filterable: false,
+                  width: 100,
+                  accessor: "links.0.href",
+                  Cell: ({ row, value }) => (
+                    <EditCustomers
+                      updateCustomer={this.updateCustomer}
+                      link={value}
+                      customer={row}
+                    />
+                  )
+                },
+                {
+                  id: "button",
+                  sortable: false,
+                  filterable: false,
+                  width: 100,
+                  accessor: "links.0.href",
+                  Cell: ({ value }) => (
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => {
+                        this.deleteCustomer(value);
+                      }}
+                    >
+                      Remove
+                    </button>
+                  )
                 }
-            ]
-        })
-    }
-
-    // UPDATES CUSTOMER
-    updateCustomer = (link, editcustomer) => {
-        fetch(link, {
-            method: 'PUT',
-            headers: {'Content-Type' : 'application/json'},
-            body: JSON.stringify(editcustomer)
-        })
-        .then(response => this.showCustomers())
-        .catch(err => console.error(err))
-
-        toast.success("Updated", {
-            position: toast.POSITION.TOP_CENTER
-        });
-    }
-
-    // ADDS TRAINING
-    addTrainings = (newTraining) => {
-        fetch('https://customerrest.herokuapp.com/api/trainings', {
-            method: 'POST',
-            headers: {
-                'Content-Type' : 'application/json',
-            },
-            body: JSON.stringify(newTraining)
-        })
-        toast.success("Training Added!", {
-            position: toast.POSITION.TOP_CENTER
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <div className = "row">
-                <AddCustomers addCustomers={this.addCustomers} />
-                </div>
-                <ReactTable
-                data={this.state.customers}
-                columns = {[
-                    {
-                        columns: [
-                            {
-                                Header: "FirstName",
-                                accessor: "firstname"
-                            },
-                            {
-                                Header: "LastName",
-                                accessor: "lastname"
-                            },
-                            {
-                                Header: "StreetAddress",
-                                accessor: "streetaddress"
-                            },
-                            {
-                                Header: "PostCode",
-                                accessor: "postcode"
-                            },
-                            {
-                                Header: "City",
-                                accessor: "city"
-                            },
-                            {
-                                Header: "Email",
-                                accessor: "email"
-                            },
-                            {
-                                Header: "Phone",
-                                accessor: "phone"
-                            },
-                            {
-                                id: 'addTraining',
-                                sortable: false,
-                                filterable: false,
-                                width: 170,
-                                accessor: 'links.0.href',
-                                Cell: ({value}) => (<AddTrainings  addTrainings={this.addTrainings} link={value}/>)
-                            },
-                            {
-                                id: 'button',
-                                sortable: false,
-                                filterable: false,
-                                width: 100,
-                                accessor: 'links.0.href',
-                                Cell: ({row,value}) => (<EditCustomers updateCustomer={this.updateCustomer} link={value} customer={row} />)
-                            },
-                            {
-                                id: 'button',
-                                sortable: false,
-                                filterable: false,
-                                width: 100,
-                                accessor: "links.0.href",
-                                Cell: ({value}) =>  (<button className="btn btn-danger" onClick={() => {this.deleteCustomer(value)}}>Remove</button>)
-                            },
-                        ]
-                    }
-                ]}
-
-                defaultSorted={[
-                    {
-                      id: "firstname",
-                      desc: true
-                    }
-                  ]}
-
-                defaultPageSize={10}
-                className="-striped-highlight"
-                filterable
-                />
-                 <ToastContainer autoClose={1500} />
-            </div>
-        );
-    }
+              ]
+            }
+          ]}
+          defaultSorted={[
+            {
+              id: "firstname",
+              desc: true
+            }
+          ]}
+          defaultPageSize={10}
+          className="-striped-highlight"
+          filterable
+        />
+        <ToastContainer autoClose={1500} />
+      </div>
+    );
+  }
 }
 
 export default Customers;
